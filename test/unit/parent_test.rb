436d8e1bd45c72adcacbcf9b4659a937f28bd360
Fernando Raganhan Barbosa <fernandorb86@gmail.com>
2015-03-17 23:12:21 -0300

    fixing tests and commenting on the weirdness

--- test/unit/parent_test.rb
+++ test/unit/parent_test.rb
@@ -4,30 +4,37 @@ class ParentTest < ActiveSupport::TestCase
   test "simple join" do
     parents_with_children = Parent.joins(:children)
 
-    assert_equal(1, parents_with_children.size)
+    # duplicate parents because of the INNER JOIN :(
+    assert_equal(2, parents_with_children.size)
   end
 
   test "with include" do
     parents_with_children = Parent.includes(:children).joins(:children)
 
-    assert_equal(1, parents_with_children.size)
+    # the size here changes the query to a count duplicating the parents
+    assert_equal(2, parents_with_children.size)
   end
 
   test "with include and all to force query" do
     parents_with_children = Parent.includes(:children).joins(:children).all
 
+    # the extra info from the include helps rails remove the duplication
+    # but you query more data than needed
     assert_equal(1, parents_with_children.size)
   end
 
   test "with group" do
+    p = parents(:with_children)
     parents_with_children = Parent.joins(:children).group('parents.id')
 
-    assert_equal(1, parents_with_children.size)
+    # the size here changes the query to a count duplicating the parents
+    assert_equal({ p.id => 2 }, parents_with_children.size)
   end
 
   test "with group and all to force query" do
     parents_with_children = Parent.joins(:children).group('parents.id').all
 
+    # works, my prefered way to deal with it
     assert_equal(1, parents_with_children.size)
   end
 end
